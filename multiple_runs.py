431f62df8f34d1f9d832d85e61cb52a1870a606a
Ziya Alim <148337611+Babur-Jihanshahi@users.noreply.github.com>
2024-01-30 13:47:41 +0100

    added varying refractory period

--- multiple_runs.py
+++ multiple_runs.py
@@ -2,13 +2,24 @@ from sandpile import BTW
 from config import kwargs_oscillatory
 
 # Varying parameter
-height_values = range(1, 10)
+# height_values = range(1, 10)
 
-for height in height_values:
+# for height in height_values:
+#     current_kwargs = kwargs_oscillatory.copy()
+#     current_kwargs['height'] = height
+
+#     btw = BTW(grid_size=[50, 50], **current_kwargs)
+
+#     btw.run(10000)
+#     btw.write_data(f"data/varying_height_{height}.csv")
+
+refractory_period_values = range(1, 10)
+
+for refractory_period in refractory_period_values:
     current_kwargs = kwargs_oscillatory.copy()
-    current_kwargs['height'] = height
+    current_kwargs['refractory_period'] = refractory_period
 
     btw = BTW(grid_size=[50, 50], **current_kwargs)
 
     btw.run(10000)
-    btw.write_data(f"data/varying_height_{height}.csv")
+    btw.write_data(f"data/varying_refractory_{refractory_period}.csv")
